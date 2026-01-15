568227b4c0739440c6d09c7576443db21ce3d544
Curtis Wei <iscurt.w@gmail.com>
2026-01-15 09:39:31 -0800

    Added files for baseline models

--- python/utils/__init__.py
+++ python/utils/__init__.py
@@ -5,5 +5,28 @@ Reusable modules for hockey prediction models.
 """
 
 from .elo_model import EloModel
+from .baseline_model import (
+    BaselineModel,
+    GlobalMeanBaseline,
+    TeamMeanBaseline,
+    HomeAwayBaseline,
+    MovingAverageBaseline,
+    WeightedHistoryBaseline,
+    PoissonBaseline,
+    compare_baselines
+)
 
-__all__ = ['EloModel']
+__all__ = [
+    # ELO Model (Model 2)
+    'EloModel',
+    
+    # Baseline Models (Model 1)
+    'BaselineModel',
+    'GlobalMeanBaseline',
+    'TeamMeanBaseline',
+    'HomeAwayBaseline',
+    'MovingAverageBaseline',
+    'WeightedHistoryBaseline',
+    'PoissonBaseline',
+    'compare_baselines'
+]
